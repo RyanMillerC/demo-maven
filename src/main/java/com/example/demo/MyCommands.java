cdfc520d5cd8a2680e6b5f74a957f022658f8e95
Ryan Miller <miller@redhat.com>
2022-03-24 09:08:21 -0400

    Add list command and Person class
    `list` will display a list of Person instances.

--- src/main/java/com/example/demo/MyCommands.java
+++ src/main/java/com/example/demo/MyCommands.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellComponent;
 
@@ -16,4 +17,12 @@ public class MyCommands {
         return a - b;
     }
 
+    @ShellMethod("List all people.")
+    public ArrayList<Person> list() {
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Ryan", 45));
+        people.add(new Person("Logan", 64));
+        people.add(new Person("Maxwell", 85));
+        return people;
+    }
 }
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Person {
+
+	public String name;
+	public int age;
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString() {
+		return String.format("Name: %s, Age: %x", this.name, this.age);
+	}
+
+}
